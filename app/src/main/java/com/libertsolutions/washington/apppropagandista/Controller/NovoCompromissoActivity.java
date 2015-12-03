b27cedc4aeef4a8c19ccfb269c25eb575f60d9c4
Filipe Bezerra <filipebzerra@gmail.com>
2015-12-03 01:00:02 -0200

    Refatoração da activity Agenda_Cadastrar para NovoCompromissoActivity.

--- app/src/main/java/com/libertsolutions/washington/apppropagandista/Controller/NovoCompromissoActivity.java
+++ app/src/main/java/com/libertsolutions/washington/apppropagandista/Controller/NovoCompromissoActivity.java
@@ -22,7 +22,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class Agenda_cadastrar extends AppCompatActivity {
+public class NovoCompromissoActivity extends AppCompatActivity {
     private AgendaDAO agendaDb;
     EditText txtData;
     EditText txtHora;
@@ -36,7 +36,7 @@ public class Agenda_cadastrar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agenda_cadastrar);
+        setContentView(R.layout.activity_novo_compromisso);
 
         //Recupera Campos
         getCampos();
@@ -48,7 +48,7 @@ public class Agenda_cadastrar extends AppCompatActivity {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (hasFocus)
-                    Tela.AbrirTela(Agenda_cadastrar.this, ConsultarMedicoActivity.class,1);
+                    Tela.AbrirTela(NovoCompromissoActivity.this, ConsultarMedicoActivity.class,1);
             }
         });
 
