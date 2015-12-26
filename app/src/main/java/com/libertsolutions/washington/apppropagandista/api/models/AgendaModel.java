aec1773ca3938f10e65de51b15b99d0d49fb2e45
Filipe Bezerra <filipebzerra@gmail.com>
2015-12-26 15:35:26 -0200

    Documentação da classe AgendaMode.

--- app/src/main/java/com/libertsolutions/washington/apppropagandista/api/models/AgendaModel.java
+++ app/src/main/java/com/libertsolutions/washington/apppropagandista/api/models/AgendaModel.java
@@ -1,15 +1,15 @@
 package com.libertsolutions.washington.apppropagandista.api.models;
 
 import com.google.gson.annotations.SerializedName;
+import com.libertsolutions.washington.apppropagandista.Model.Agenda;
 
 /**
  * Classe modelo para envio para e recebimento de dados do webservice. <br><br>
  *
- * A fonte dos dados enviados são obtidas a partir da entidade
- * {@link com.libertsolutions.washington.apppropagandista.Model.Agenda}. <br><br>
+ * A fonte dos dados enviados são obtidas a partir do método {@link Agenda#toModel(Agenda)}. <br><br>
  *
- * Os dados recebidos do webservice são
- *
+ * Os dados recebidos do webservice são armazenados localmente usando o método
+ * {@link Agenda#fromModel(AgendaModel)}.
  *
  * @author Filipe Bezerra
  * @version 1.0, 24/12/2015
