d1c57e5b2ec9c193f23c91fbabccb0db2572a061
Filipe Bezerra <filipebzerra@gmail.com>
2015-12-27 01:27:22 -0200

    Configurações com prévia das configurações já feitas.

--- app/src/main/java/com/libertsolutions/washington/apppropagandista/Controller/ConfiguracoesFragment.java
+++ app/src/main/java/com/libertsolutions/washington/apppropagandista/Controller/ConfiguracoesFragment.java
@@ -1,10 +1,14 @@
 package com.libertsolutions.washington.apppropagandista.Controller;
 
 import android.os.Bundle;
-import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceManager;
 import com.libertsolutions.washington.apppropagandista.R;
+
+import static com.libertsolutions.washington.apppropagandista.Util.PreferencesUtils.PREF_AUTH_KEY;
+import static com.libertsolutions.washington.apppropagandista.Util.PreferencesUtils.PREF_SERVER_URL;
 
 /**
  * Tela das configurações.
@@ -13,21 +17,38 @@ import com.libertsolutions.washington.apppropagandista.R;
  * @version 1.0, 20/11/2015
  * @since 1.0
  */
-public class ConfiguracoesFragment extends PreferenceFragmentCompat
-        implements Preference.OnPreferenceChangeListener {
-
+public class ConfiguracoesFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preferences);
-        findPreference("pref_url").setOnPreferenceChangeListener(this);
-        findPreference("pref_auth_key").setOnPreferenceChangeListener(this);
+        bindPreferenceSummaryToValue(findPreference(PREF_SERVER_URL));
+        bindPreferenceSummaryToValue(findPreference(PREF_AUTH_KEY));
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object o) {
-        if (preference instanceof EditTextPreference) {
-            preference.setSummary((String)o);
+    private static Preference.OnPreferenceChangeListener sOnPreferenceChangeListener
+            = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            String strinValue = o.toString();
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                final int index = listPreference.findIndexOfValue(strinValue);
+                preference.setSummary(index >= 0 ?
+                        listPreference.getEntries()[index] : null);
+            } else {
+                preference.setSummary(strinValue);
+            }
+
+            return true;
         }
-        return true;
+    };
+
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(sOnPreferenceChangeListener);
+        sOnPreferenceChangeListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
     }
 }
